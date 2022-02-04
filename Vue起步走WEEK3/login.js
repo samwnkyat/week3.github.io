2392be7168462f83e0fe7e63e742a7d85899abbf
samwnkyat <91939489+samwnkyat@users.noreply.github.com>
2022-02-04 19:26:44 +0800

    Add files via upload

--- Vue起步走WEEK3/login.js
+++ Vue起步走WEEK3/login.js
@@ -3,25 +3,22 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 createApp({
     data() {
         return {
-            apiUrl: 'https://vue3.course-api.hexschool.io/v2',
             user: {
                 username: '',
                 password: '',
             },
-        };
+        }
     },
     methods: {
         login() {
-            const api = `${this.apiUrl}/admin/signin`;
-
+            const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
             axios.post(api, this.user).then((response) => {
                 const { token, expired } = response.data;
-                //寫入cookie token
-                //expires 設置有效時間
-                document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/}`;
-
+                // 寫入 cookie token
+                // expires 設置有效時間
+                document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
                 window.location = 'products.html';
-            }).cathc((error) => {
+            }).catch((error) => {
                 alert(error.data.message);
             });
         },
